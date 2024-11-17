7629590079933134a17710d7c56aede6f40c891c
andndre <agungandre687@gmail.com>
2024-11-18 06:40:04 +0800

    fix nav user profile

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -12,7 +12,7 @@
             <li class="nav-item navbar-dropdown dropdown-user dropdown">
                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                     <div class="avatar avatar-online">
-                        <img src="{{ auth()->user()->profile_picture ? Storage::url(auth()->user()->profile_picture) : asset('assets/img/avatars/placeholder.jpg') }}" alt class="w-px-40 h-auto rounded-circle" />
+                        <img src="{{ auth()->user()->profile_picture ? auth()->user()->profile_picture : asset('assets/img/avatars/placeholder.jpg') }}" alt class="w-px-40 h-auto rounded-circle" />
                     </div>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end">
@@ -21,7 +21,7 @@
                             <div class="d-flex">
                                 <div class="flex-shrink-0 me-3">
                                     <div class="avatar avatar-online">
-                                        <img src="{{ auth()->user()->profile_picture ? Storage::url(auth()->user()->profile_picture) : asset('assets/img/avatars/placeholder.jpg') }}" alt=""
+                                        <img src="{{ auth()->user()->profile_picture ? auth()->user()->profile_picture : asset('assets/img/avatars/placeholder.jpg') }}" alt=""
                                             class="w-px-40 h-auto rounded-circle" />
                                     </div>
                                 </div>
